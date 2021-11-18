cfa769603c677232d1b045a2599c89e7d16beccc
Trajanus <derektgoodwin@gmail.com>
2021-11-18 08:36:08 -0800

    Switched from using track length to cut tracks to bytes written.

--- AudioStreamPoc/PlaylistCapture.cs
+++ AudioStreamPoc/PlaylistCapture.cs
@@ -3,10 +3,8 @@ using CSCore.SoundIn;
 using log4net;
 using PlaylistsNET.Models;
 using System;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Threading;
 
 namespace AudioStreamPoc
 {
@@ -18,17 +16,15 @@ namespace AudioStreamPoc
         {
             _playlist = playlist;
             _capture = new WasapiLoopbackCapture();
-            _recordingTimer = new Stopwatch();
         }
 
         private WasapiCapture _capture;
         private WaveWriter _writer;
         private M3uPlaylist _playlist;
         private int _playlistIndex = 0;
-        private Stopwatch _recordingTimer;
 
         private readonly long BytesWrittenThreshold = 100000;
-        private long bytesWritten = 0;
+        private long trackBytesWritten = 0;
 
         public void StartCapture()
         {
@@ -48,47 +44,60 @@ namespace AudioStreamPoc
             _capture.Dispose();
         }
 
+        private void StartNewTrackRecording(double numBytesInTrack)
+        {
+            string filename = GetFileName(_playlist.PlaylistEntries[_playlistIndex]);
+
+            if (File.Exists(filename))
+            {
+                File.Delete(filename);
+            }
+
+            _writer = new WaveWriter(filename, _capture.WaveFormat);
+            Log.Debug($"Started recording track: {_playlist.PlaylistEntries[_playlistIndex].Title}; expected bytes in track: {numBytesInTrack}.");
+        }
+
         private void DataAvailable(object s, DataAvailableEventArgs e)
         {
-            double totalTrackTicks = _playlist.PlaylistEntries[_playlistIndex].Duration.TotalMilliseconds * TimeSpan.TicksPerMillisecond;
-            if (_recordingTimer.ElapsedTicks >= totalTrackTicks)
+            double numBytesInTrack = (_playlist.PlaylistEntries[_playlistIndex].Duration.TotalMilliseconds / 1000) * e.Format.BytesPerSecond;
+            if (null == _writer || _writer.IsDisposed)
             {
-                _writer.Write(e.Data, e.Offset, e.ByteCount);
-
-                if (!_writer.IsDisposed)
-                {
-                    _writer.Dispose();
-
-                    if (bytesWritten >= BytesWrittenThreshold)
-                    {
-                        bytesWritten = 0;
-                        _playlistIndex++;
-                        _recordingTimer.Restart();
-                        if(_playlistIndex >= _playlist.PlaylistEntries.Count())
-                        {
-                            _capture.DataAvailable -= DataAvailable;
-                        }
-                    }
-                    else
-                    {
-                        string filename = GetFileName(_playlist.PlaylistEntries[_playlistIndex]);
-                        if (File.Exists(filename))
-                        {
-                            File.Delete(filename);
-                        }
-                    }
-                }
+                StartNewTrackRecording(numBytesInTrack);
             }
-            else
+
+            int remainingBytesInTrack = (int)(numBytesInTrack - trackBytesWritten);
+            int bufferSize = remainingBytesInTrack > e.ByteCount ? e.ByteCount : remainingBytesInTrack;
+
+            byte[] buffer = new byte[bufferSize];
+            Buffer.BlockCopy(e.Data, 0, buffer, 0, bufferSize);
+            _writer.Write(buffer, 0, bufferSize);
+            trackBytesWritten += bufferSize;
+
+            bool allTrackDataWritten = bufferSize != e.ByteCount;
+
+            if (allTrackDataWritten && trackBytesWritten >= BytesWrittenThreshold)
             {
-                if (null == _writer || _writer.IsDisposed)
+                _writer.Dispose();
+                trackBytesWritten = 0;
+                _playlistIndex++;
+                if (_playlistIndex >= _playlist.PlaylistEntries.Count())
                 {
-                    string filename = GetFileName(_playlist.PlaylistEntries[_playlistIndex]);
-                    _writer = new WaveWriter(filename, _capture.WaveFormat);
-                    _recordingTimer.Start();
+                    _capture.DataAvailable -= DataAvailable;
                 }
-                _writer.Write(e.Data, e.Offset, e.ByteCount);
-                bytesWritten += e.ByteCount;
+
+                if (bufferSize < e.ByteCount)
+                {
+                    numBytesInTrack = (long)(_playlist.PlaylistEntries[_playlistIndex].Duration.TotalMilliseconds / 1000) * e.Format.BytesPerSecond;
+                    StartNewTrackRecording(numBytesInTrack);
+
+                    int offSet = bufferSize;
+                    int newBufferSize = e.ByteCount - bufferSize;
+
+                    byte[] newBuffer = new byte[newBufferSize];
+                    Buffer.BlockCopy(e.Data, bufferSize, newBuffer, 0, newBufferSize);
+                    _writer.Write(newBuffer, 0, newBufferSize);
+                    trackBytesWritten += newBufferSize;
+                }
             }
         }
 
